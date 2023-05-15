24bcaef3d0723f19456212b198c10f08db889d96
APTTUS\nnaik <nnaik@conga.com>
2023-05-15 17:46:55 +0530

    Upgrade Activity for sep22-june23

--- src/app/modules/quote/component/request-quote-form/request-quote-form.component.ts
+++ src/app/modules/quote/component/request-quote-form/request-quote-form.component.ts
@@ -58,9 +58,6 @@ export class RequestQuoteFormComponent implements OnInit {
       });
   }
 
-  /**
-   * This method adds comments to requesting quote.
-   */
   addComment() {
     if (this.quote) {
       this.quote.Description = this.note.Body;
@@ -68,31 +65,40 @@ export class RequestQuoteFormComponent implements OnInit {
     }
   }
 
-  /**
-   * @ignore
-   */
   quoteChange() {
     this.onQuoteUpdate.emit(this.quote);
   }
 
   shipToChange() {
-    this.shipToAccount$ = this.accountService.getAccount(this.quote.ShipToAccountId);
-    this.shipToAccount$.pipe(take(1)).subscribe((newShippingAccount) => {
-      this.quote.ShipToAccount = newShippingAccount;
+    if (this.quote.ShipToAccountId) {
+      this.shipToAccount$ = this.accountService.getAccount(this.quote.ShipToAccountId);
+      this.shipToAccount$.pipe(take(1)).subscribe((newShippingAccount) => {
+        this.quote.ShipToAccount = newShippingAccount;
+        this.onQuoteUpdate.emit(this.quote);
+      });
+    } else {
+      this.quote.ShipToAccount = null;
+      this.shipToAccount$= null;
       this.onQuoteUpdate.emit(this.quote);
-    });
+    }
   }
 
   billToChange() {
-    this.billToAccount$ = this.accountService.getAccount(this.quote.BillToAccountId);
-    this.billToAccount$.pipe(take(1)).subscribe((newBillingAccount) => {
-      this.quote.BillToAccount = newBillingAccount;
+    if (this.quote.BillToAccountId) {
+      this.billToAccount$ = this.accountService.getAccount(this.quote.BillToAccountId);
+      this.billToAccount$.pipe(take(1)).subscribe((newBillingAccount) => {
+        this.quote.BillToAccount = newBillingAccount;
+        this.onQuoteUpdate.emit(this.quote);
+      });
+    } else {
+      this.quote.BillToAccount = null;
+      this.billToAccount$= null;
       this.onQuoteUpdate.emit(this.quote);
-    });
-
+    }
   }
 
-  primaryContactChange() {
+primaryContactChange() {
+  if (this.contactId) {
     this.contactService.fetch(this.contactId)
       .pipe(take(1))
       .subscribe((newPrimaryContact: Contact) => {
@@ -100,7 +106,11 @@ export class RequestQuoteFormComponent implements OnInit {
         this.quote.Primary_ContactId = newPrimaryContact.Id;
         this.onQuoteUpdate.emit(this.quote);
       });
+  } else {
+    this.quote.Primary_Contact = null;
+    this.onQuoteUpdate.emit(this.quote);
   }
+}
 
   /**
    * Event handler for when the primary contact input changes.
